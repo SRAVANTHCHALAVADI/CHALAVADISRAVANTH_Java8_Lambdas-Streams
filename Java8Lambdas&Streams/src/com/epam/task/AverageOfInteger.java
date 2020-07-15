0e616694579008935f065ccaeb8437c4420676aa
Sravanth Chalavadi <Sravanth Chalavadi@DESKTOP-I4NO74C>
2020-07-15 17:14:25 +0530

    Java 8 Lambdas & Streams

---
+++ Java8Lambdas&Streams/src/com/epam/task/AverageOfInteger.java
@@ -0,0 +1,19 @@
+package com.epam.task;
+
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
+public class AverageOfInteger {
+	public static void main(String[] args) 
+    {
+	 	int a[]= {2,3,4,5,6,7,8,9};
+        IntStream stream = IntStream.of(a); 
+        OptionalDouble obj = stream.average(); 
+        if (obj.isPresent()) { 
+            System.out.println(obj.getAsDouble()); 
+        } 
+        else { 
+            System.out.println("-1"); 
+        } 
+    } 
+}
